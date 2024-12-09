a62d67a1edb4689c5745824a6236c89d0ef5fa86
loingtan <0939899928za.com@gmail.com>
2024-12-09 15:53:37 +0700

    feat: final v2

--- src/hooks/useReadFileFromPublic.ts
+++ src/hooks/useReadFileFromPublic.ts
@@ -22,28 +22,24 @@ function useReadFileFromPublic() {
     }, []);
 
     useEffect(() => {
-        // Check if page has been refreshed using localStorage
+
         const isPageRefreshed = localStorage.getItem("isPageRefreshed");
 
         if (!isPageRefreshed && fileContent) {
             const lines = fileContent.split(/\r?\n/);
-            // Shuffle the array if the page is refreshed
-            const shuffled = shuffleArray(lines);
+            const shuffled = shuffleArray(lines).slice(0, 30);
             setShuffledContent(shuffled);
-
-            // Set a flag in localStorage to prevent shuffling on state change
             localStorage.setItem("isPageRefreshed", "true");
         } else if (fileContent) {
-            setShuffledContent(fileContent.split(/\r?\n/)); // Keep original content if not a fresh page load
+            setShuffledContent(fileContent.split(/\r?\n/).slice(0, 30));
         }
 
-        // Optionally, remove the "isPageRefreshed" flag if you want reshuffling on future refreshes
+
         return () => {
             localStorage.removeItem("isPageRefreshed");
         };
     }, [fileContent]);
 
-    // Helper function to shuffle the array
     const shuffleArray = (array: string[]) => {
         return array.sort(() => Math.random() - 0.5);
     };
